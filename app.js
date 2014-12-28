dabadb3cbfecc91384cddfda78f37da417e837b1
Pocket <p0ck31@gmail.com>
2014-12-28 13:46:06 -0800

    Get a step closer

--- app.js
+++ app.js
@@ -8,11 +8,15 @@ app.get('/', function (req, res) {
 
 // I put in the bones of my node
 
-var server = app.listen(3000, function () {
+app.post('/posting', function(req, res) {
+  var displayText = req.body.textSpot;
+  var displayImage = req.body.imgSpot;
+  console.log("REQUEST RECIEVED : " + displayText + ":and also text:" + displayImage);
+  res.send();
+});
 
+var server = app.listen(3000, function () {
   var host = server.address().address
   var port = server.address().port
-
   console.log('Example app listening at http://%s:%s', host, port)
-
 })
